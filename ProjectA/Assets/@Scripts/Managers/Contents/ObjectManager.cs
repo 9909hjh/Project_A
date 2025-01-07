488b0461cbd1872c8b1fd8f7df7ac91073e7cab1
9909hjh <9909hjh@gmail.com>
2025-01-07 19:29:26 +0900

    버프 및 디버프 등 이펙트 코드 제작

--- ProjectA/Assets/@Scripts/Managers/Contents/ObjectManager.cs
+++ ProjectA/Assets/@Scripts/Managers/Contents/ObjectManager.cs
@@ -9,6 +9,7 @@ public class ObjectManager
     public HashSet<Monster> Monsters { get; } = new HashSet<Monster>();
     public HashSet<Projectile> Projectiles { get; } = new HashSet<Projectile>();
     public HashSet<Env> Envs { get; } = new HashSet<Env>();
+    public HashSet<EffectBase> Effects { get; } = new HashSet<EffectBase>();
     public HeroCamp Camp { get; private set; }
 
     #region Root
@@ -25,6 +26,7 @@ public class ObjectManager
     public Transform MonsterRoot { get { return GetRootTransform("@Monsters"); } }
     public Transform ProjectileRoot { get { return GetRootTransform("@Projectiles"); } }
     public Transform EnvRoot { get { return GetRootTransform("@Envs"); } }
+    public Transform EffectRoot { get { return GetRootTransform("@Effects"); } }
     #endregion
 
     // 데미지 폰트
@@ -33,6 +35,19 @@ public class ObjectManager
         GameObject go = Managers.Resource.Instantiate("DamageFont", pooling: true);
         DamageFont damageText = go.GetComponent<DamageFont>();
         damageText.SetInfo(position, damage, parent, isCritical);
+    }
+
+    public GameObject SpawnGameObject(Vector3 position, string prefabName)
+    {
+        GameObject go = Managers.Resource.Instantiate(prefabName, pooling: true);
+        go.transform.position = position;
+        return go;
+    }
+
+    public T Spawn<T>(Vector3Int cellPos, int templateID) where T : BaseObject
+    {
+        Vector3 spawnPos = Managers.Map.Cell2World(cellPos);
+        return Spawn<T>(spawnPos, templateID);
     }
 
     public T Spawn<T>(Vector3 position, int templateID) where T : BaseObject
